6b890f9bf97c393af6f019a2cef014a13b281109
Bryan Van Horn <bvhwa@yahoo.com>
2019-06-08 15:31:56 -0700

    Added TutorialScene.swift file so that user is required to play tutorial initially to get how game is played quickly before playing

--- CollectItemsGame/GameScene.swift
+++ CollectItemsGame/GameScene.swift
@@ -11,12 +11,10 @@ import GameplayKit
 
 class GameScene: SKScene {
     var player: SKSpriteNode!
-    var ground: SKSpriteNode!
     
     override func didMove(to view: SKView) {
         initializeUI()
         initializeBackground()
-        initializeGround()
         initializePlayer()
     }
     
@@ -24,19 +22,37 @@ class GameScene: SKScene {
         // Called before each frame is rendered
     }
     
-    private func initializeUI() {
+    func initializeUI() {
         // Steven can do UI
     }
     
-    private func initializeBackground() {
-        // Load up background asset
+    func initializeBackground() {
+        // Load up background asset with a level ground for player to stand on
+        // let background = SKSpriteNode(imageNamed: "Background")
+        // Add background asset to view
+        /*
+        self.addChild(background)
+        */
+        // For testing purposes set scene's background color to white
+        self.backgroundColor = SKColor.white
     }
     
-    private func initializeGround() {
-        // Load up ground asset for player to stand on
-    }
-    
-    private func initializePlayer() {
-        // Load up player asset
+    func initializePlayer() {
+        // Load up player asset as brown rectangle
+        player = SKSpriteNode(color: SKColor.brown, size: CGSize(width: 40, height: 100))
+        // Set the player's initial position
+        player.position = CGPoint(x: size.width / 2, y: (size.height / 2) - 225)
+        // Set the player's position to be in the foreground rather than background
+        player.zPosition = 1
+        // Add physics body to the player to interact with other objects
+        player.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: player.size.width, height: player.size.height))
+        // Turn off gravity for the player so he doesn't fall through the ground
+        player.physicsBody?.affectedByGravity = false
+        // Don't allow physics to affect player
+        player.physicsBody?.isDynamic = false
+        // Don't allow the player to experience friction
+        player.physicsBody?.friction = 0.0
+        // Add the player to the scene
+        self.addChild(player)
     }
 }

67e88c2f98bd401517e64ccd794d3ef4e031e3b7
Bryan Van Horn <bvhwa@yahoo.com>
2019-06-08 15:36:43 -0700

    Added TutorialScene.swift file since last commit didn't capture it

---
+++ CollectItemsGame/TutorialScene.swift
@@ -0,0 +1,22 @@
+//
+//  TutorialScene.swift
+//  CollectItemsGame
+//
+//  Created by Bryan Van Horn on 6/8/19.
+//  Copyright © 2019 Bryan Van Horn. All rights reserved.
+//
+import SpriteKit
+import GameplayKit
+
+class TutorialScene: GameScene {
+    
+    override func didMove(to view: SKView) {
+        super.initializeUI()
+        super.initializeBackground()
+        super.initializePlayer()
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        
+    }
+}
